5a7752d785d44a4d639a458d4e900437816cc510
Alasdair Mott <alasdair.c.mott@gmail.com>
2021-10-09 15:00:24 +1100

    Update CVOscillatorComponent.cs

--- Siren/Audio/CVOscillatorComponent.cs
+++ Siren/Audio/CVOscillatorComponent.cs
@@ -107,7 +107,7 @@ namespace Siren.Components
 		/// <summary>
 		/// Provides an Icon for the component.
 		/// </summary>
-		protected override System.Drawing.Bitmap Icon => null;
+		protected override System.Drawing.Bitmap Icon => Properties.Resources.sin;
 
 		/// <summary>
 		/// Gets the unique ID for this component. Do not change this ID after release.
