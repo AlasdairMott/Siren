945dd4f1e34f1561afd62fba86e901de9a153ee1
Alasdair Mott <alasdair.c.mott@gmail.com>
2021-10-12 07:40:38 +1100

    Update TriggeredSampleProvider.cs
    - Simplify triggered sample logic

--- Siren/SampleProviders/TriggeredSampleProvider.cs
+++ Siren/SampleProviders/TriggeredSampleProvider.cs
@@ -10,60 +10,46 @@ namespace Siren.SampleProviders
 	{
 		private CachedSound sample;
 		private ISampleProvider pulses;
-		private List<float> cache;
+		private Queue<float> cache;
 
 		public WaveFormat WaveFormat => sample.WaveFormat;
 		public long Length { get; private set; }
-		public int Position { get; set; }
 
 		public TriggeredSampleProvider(CachedSound sample, ISampleProvider pulses) 
 		{
 			this.sample = sample;
 			this.pulses = pulses;
-			cache = new List<float>();
-
-			Position = 0;
+			cache = new Queue<float>();
 		}
 
 		public int Read(float[] buffer, int offset, int count)
 		{
-			//var pulseBuffer = new float[count];
-
-			int samplesRead = pulses.Read(buffer, offset, count);
-			if (samplesRead == 0) return 0;
-
-			var outBuffer = new float[samplesRead + sample.AudioData.Length];
-			Array.Copy(cache.ToArray(), 0, outBuffer, 0, Math.Min(cache.Count, outBuffer.Length));
-			cache.Clear();
+			var pulseBuffer = new float[count];
+			int samplesRead = pulses.Read(pulseBuffer, offset, count); //read the pulse signal
+			if (samplesRead == 0)
+			{
+				if (cache.Count == 0) return 0;
+				else if (samplesRead == 0) samplesRead = Math.Min(cache.Count, count);
+			}
 
 			bool triggered = false;
 			for (int n = 0; n < samplesRead; n++)
 			{
-				var value = buffer[offset + n];
-				if (value < 0.1) triggered = false;
-				else if (value > 0.1 & !triggered)
+				float p = 0f;
+				if (n < pulseBuffer.Length)
+					p = pulseBuffer[offset + n];
+
+				if (p < 0.1) triggered = false;
+				else if (p > 0.1 & !triggered)
 				{
 					triggered = true;
-
-					var location = n - Position;
-					var remaining = buffer.Count() - location;
-					var length = Math.Min(sample.AudioData.Length, remaining);
-
-					if (length < sample.AudioData.Length) 
-					{ 
-						var samplesToCache = new float[sample.AudioData.Length - length];
-						Array.Copy(sample.AudioData, length, samplesToCache, 0, samplesToCache.Length);
-						cache.AddRange(samplesToCache);
-					}
-
-					Array.Copy(sample.AudioData, 0, outBuffer, offset + n, length);
+					cache = new Queue<float>(sample.AudioData); //fill the cache with sample audio
+				}
+				if (cache.Count > 0)
+				{
+					buffer[n + offset] = cache.Dequeue(); //replace the buffer with audio from the cache
 				}
 			}
-			//buffer = outBuffer.Take(samplesRead).ToArray();
-
-			Array.Copy(outBuffer, 0, buffer, 0, Math.Min(buffer.Length, outBuffer.Length));
-
-			Position += count;
 
 			return samplesRead;
 		}
