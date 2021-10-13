16c6ba8e31e6e262f09434fd5fd5175b1f9c1f37
Alasdair Mott <alasdair.c.mott@gmail.com>
2021-10-13 18:51:46 +1100

    simple Decay envelope
    - Decay only

---
+++ Siren/Audio/ADEnvelopeComponent.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Siren.Audio
+{
+	public class ADEnvelopeComponent : GH_Component
+	{
+		/// <summary>
+		/// Initializes a new instance of the ADEnvelopeComponent class.
+		/// </summary>
+		public ADEnvelopeComponent()
+		  : base("AD Envelope Generator", "Nickname",
+			  "Attack/Decay Envelope Generator",
+			  "Siren", "Envelope")
+		{
+		}
+
+		/// <summary>
+		/// Registers all the input parameters for this component.
+		/// </summary>
+		protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+		{
+			pManager.AddParameter(new WaveStreamParameter(), "Wave", "W", "Wave input", GH_ParamAccess.item);
+			pManager.AddNumberParameter("Attenuation", "A", "Attenuation", GH_ParamAccess.item);
+		}
+
+		/// <summary>
+		/// Registers all the output parameters for this component.
+		/// </summary>
+		protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+		{
+			pManager.AddParameter(new WaveStreamParameter(), "Wave", "W", "Wave input", GH_ParamAccess.item);
+		}
+
+		/// <summary>
+		/// This is the method that actually does the work.
+		/// </summary>
+		/// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+		protected override void SolveInstance(IGH_DataAccess DA)
+		{
+			var waveIn = CachedSound.Empty;
+			if (!DA.GetData(0, ref waveIn)) return;
+
+			var decay = 1.0;
+			if (!DA.GetData(1, ref decay)) return;
+
+			var AD = new SampleProviders.ADEnvelopeProvider(waveIn.ToSampleProvider(), (float) decay);
+
+			DA.SetData(0, AD);
+		}
+
+		/// <summary>
+		/// Provides an Icon for the component.
+		/// </summary>
+		protected override System.Drawing.Bitmap Icon
+		{
+			get
+			{
+				//You can add image files to your project resources and access them like this:
+				// return Resources.IconForThisComponent;
+				return null;
+			}
+		}
+
+		/// <summary>
+		/// Gets the unique ID for this component. Do not change this ID after release.
+		/// </summary>
+		public override Guid ComponentGuid
+		{
+			get { return new Guid("62e6c701-bd59-4e03-9898-7bf953043b4b"); }
+		}
+	}
+}
---
+++ Siren/SampleProviders/ADEnvelopeProvider.cs
@@ -0,0 +1,60 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+
+namespace Siren.SampleProviders
+{
+	public class ADEnvelopeProvider : ISampleProvider
+	{
+		private ISampleProvider pulses;
+
+		private int attackLength;
+		private int decayLength;
+		private float power;
+		private float decay;
+
+		public WaveFormat WaveFormat => pulses.WaveFormat;
+
+		public ADEnvelopeProvider(ISampleProvider pulses, float decay)
+		{
+			this.pulses = pulses;
+			//attackLength = (int) attack.TotalSeconds * pulses.WaveFormat.SampleRate;
+			//decayLength = (int) decay.TotalSeconds * pulses.WaveFormat.SampleRate;
+			power = 0;
+			this.decay = NAudioUtilities.Clamp(decay, 0.0f, 0.9999f);
+		}
+
+		public int Read(float[] buffer, int offset, int count)
+		{
+			var pulseBuffer = new float[count];
+			int samplesRead = pulses.Read(pulseBuffer, offset, count); //read the pulse signal
+			if (samplesRead == 0)
+			{
+				if (power == 0f) return 0;
+				else if (samplesRead == 0) samplesRead = WaveFormat.SampleRate;
+			}
+
+			bool triggered = false;
+			for (int n = 0; n < samplesRead; n++)
+			{
+				float p = 0f;
+				if (n < pulseBuffer.Length)
+					p = pulseBuffer[offset + n];
+
+				if (p < 0.1) triggered = false;
+				else if (p > 0.1 & !triggered)
+				{
+					triggered = true;
+					power = 1.0f;
+				}
+				power *= decay;
+				if (power < 0.01f) power = 0.0f;
+				buffer[offset + n] = power;
+			}
+
+			return samplesRead;
+		}
+
+
+	}
+}
